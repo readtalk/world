cf68579886b5490a42158ad955ba6860d759d8e4
READTalk <redtalk.app@hotmail.com>
2026-09-19 06:43:06 +0700

    Refactor login function to remove token checks
    Removed token verification logic from the login function.

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,6 +1,4 @@
-//
 "use server"
-
 import { redirect } from "next/navigation"
 import { headers as getHeaders, cookies as getCookies } from "next/headers"
 import { subjects } from "../auth/subjects"
@@ -18,16 +16,6 @@ export async function auth() {
 }
 
 export async function login() {
-  const cookies = await getCookies()
-  const accessToken = cookies.get("access_token")
-  const refreshToken = cookies.get("refresh_token")
-  if (accessToken) {
-    const verified = await client.verify(subjects, accessToken.value, { refresh: refreshToken?.value })
-    if (!verified.err && verified.tokens) {
-      await setTokens(verified.tokens.access, verified.tokens.refresh)
-      redirect("/")
-    }
-  }
   const headers = await getHeaders()
   const host = headers.get("host")
   const protocol = host?.includes("localhost") ? "http" : "https"
