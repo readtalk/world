9a7457f0735ddd98d800712670ec5674cd5410ec
READTalk <redtalk.app@hotmail.com>
2026-09-19 05:43:47 +0700

    Refactor Home component layout and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,49 @@
-//
-import Image from "next/image"
 import { auth, login, logout } from "./actions"
 
 export default async function Home() {
   const subject = await auth()
 
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           {subject ? (
             <>
-              <li>
-                Logged in as <code>{subject.properties.id}</code>.
+              <li className="mb-2 tracking-[-.01em]">
+                Logged in as{" "}
+                <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+                  {subject.properties.id}
+                </code>
+                .
               </li>
-              <li>
-                And then check out <code>app/page.tsx</code>.
+              <li className="tracking-[-.01em]">
+                And then check out <code>src/app/page.tsx</code>.
               </li>
             </>
           ) : (
             <>
-              <li>Login with your email and password.</li>
-              <li>
-                And then check out <code>app/page.tsx</code>.
+              <li className="mb-2 tracking-[-.01em]">
+                Login with your email and password.
+              </li>
+              <li className="tracking-[-.01em]">
+                And then check out <code>src/app/page.tsx</code>.
               </li>
             </>
           )}
         </ol>
 
-        <div className={styles.ctas}>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
           {subject ? (
             <form action={logout}>
-              <button className={styles.secondary}>Logout</button>
+              <button className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]">
+                Logout
+              </button>
             </form>
           ) : (
             <form action={login}>
-              <button className={styles.primary}>Login with OpenAuth</button>
+              <button className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]">
+                Login with OpenAuth
+              </button>
             </form>
           )}
         </div>
