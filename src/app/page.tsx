bc7cdfd7811ca6a627944b07036ec05a3c767012
READTalk <redtalk.app@hotmail.com>
2026-09-19 04:48:27 +0700

    Implement authentication UI in Home component

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { auth, login, logout } from "./actions"
+
+export default async function Home() {
+  const subject = await auth()
+
+  return (
+    <div>
+      {subject ? (
+        <>
+          <p>Logged in as <code>{subject.properties.id}</code></li>
+          <form action={logout}><button>Logout</button></form>
+        </>
+      ) : (
+        <>
+          <p>Login with your email and password.</p>
+          <form action={login}><button>Login with OpenAuth</button></form>
+        </>
+      )}
+    </div>
+  )
+}
